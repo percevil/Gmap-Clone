3f6e8664ebdf31918fe6cf00ef95f259f8969681
percevil <peeevil@gmail.com>
2020-09-16 15:37:07 -0700

    map working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// Start writing JavaScript here!
+function initGoogleMap() {
+  const mapDiv = document.querySelector('#map');
+
+  // Creating the Google Map
+  new google.maps.Map(mapDiv, {
+    center: { lat: 49.1963334, lng: -122.7991537 },
+    zoom: 16,
+  });
+}
